7f196640beaa199e278a567e1b980db12ccafafe
joeyzcs <130409814+joeyzcs@users.noreply.github.com>
2024-03-20 14:24:58 -0500

    Create game.c
    First push

---
+++ game.c
@@ -0,0 +1,20 @@
+/****************************************************************
+ * File: 	game.c
+ * Author: 	Denis Alisa, Joseph Stiehm
+ * Description: Adventure game by Joseph Stiehm and Denis Alisa
+ * Date:	3/19/2024
+ ****************************************************************/
+
+
+/* Preprocessor Directives */
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/* Start of MAIN function */
+int main() {
+
+	printf("CS366 Game\n");
+
+	return 0;
+}
