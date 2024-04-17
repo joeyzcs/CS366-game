06aa84066825f4fe1074e20f2593e2f98f165568
DenoDev18 <130480806+DenoDev18@users.noreply.github.com>
2024-04-17 17:11:22 -0500

    Added a room plus run and altDescCheck function

--- CS366-game/game.c
+++ CS366-game/game.c
@@ -47,6 +47,8 @@ void gotoroom(struct Map*, char*);
 void whereami(struct Map*);
 void pickup(struct Map*);
 void inventory(struct Map*);
+void run(struct Map*);
+void altDescCheck(struct Map*);
 
 /* This function creates the definitions of all the rooms and returns the map pointer */
 struct Map* initMap() {
@@ -61,7 +63,8 @@ struct Map* initMap() {
 	map->roomsList[2].name = "Lost_Burials";
 	map->roomsList[3].name = "Soul_Forest";
 	map->roomsList[4].name = "Morgue";
-	map->roomsList[5].name = "Old_Chapel";
+	map->roomsList[5].name = "Morgue_Storage";
+	map->roomsList[6].name = "Old_Chapel";
 
 	
 	/* Description of all rooms */
@@ -70,13 +73,15 @@ struct Map* initMap() {
 
 	map->roomsList[1].description = "\nYou are currently in the Ancient_Ruins. Centuries old graves of those who died many years ago lay to rest here. You notice a Damascus Steel Sword rested upon a tombstone. It could prove to be useful, you should pick it up.\n\n";
 
-	map->roomsList[2].description = "\nYou are currently in the Lost_Burials. Those forgotten in history roam through these graves angry and undead. You might need a sword to protect yourself here.\n\n";
+	map->roomsList[2].description = "\nYou are currently in the Lost_Burials. Those forgotten in history roam through these graves angry and undead. The angry undead have noticed you. They begin to swarm you. Do you run or fight?.\n\n";
 
-	map->roomsList[3].description = "\nYou are currently in Soul_Forest. A heavily wooded area seperating the cemetery from the rest of the graveyard. Those who enter these woods rarely come out alive. Good luck.\n\n";
+	map->roomsList[3].description = "\nYou are currently in Soul_Forest. A heavily wooded area seperating the cemetery from the rest of the graveyard. Those who enter these woods rarely come out alive. You are surrouned by ghosts who crave a body to inhabit. Do you run or fight?.\n\n";
 
-	map->roomsList[4].description = "\nYou are currently at the Morgue. An overgrown house that is home to those yet to be buried. You notice a padlock on the gate. There must be a key somewhere. Lost_Burials, Soul_Forest, and the Old_Chapel are nearby.\n\n";
+	map->roomsList[4].description = "\nYou are currently at the Morgue. An overgrown house that is home to those yet to be buried. You notice a padlock on the gate. There must be a key somewhere. Use the key to enter.\n\n";
 
-	map->roomsList[5].description = "\nYou are currently at the Old_Chapel. An old structure that appears it could collapse any second. The Morgue is nearby.\n\n";
+	map->roomsList[5].description = "\nYou are inside the morgue storage room. It is filled witht the undead waiting for their time to be buried. You notice these undead look much stronger than the ones before. Do you run or fight?\n\n";
+
+	map->roomsList[6].description = "\nYou are currently at the Old_Chapel. An old structure that appears it could collapse any second. You sense victory is near and a strong urge to fight overwhelms you. You enter the chapel and at the alter you see him. The final opponent. The skeleton. Do you run or fight?\n\n";
 
 
 	/* Alternate descriptions of rooms [1], [3], and [4] */
@@ -85,7 +90,7 @@ struct Map* initMap() {
 
 	map->roomsList[3].altDescription = "You are currently in Soul_Forest. A heavily wooded area seperating the cemetery from the rest of the graveyard. Those who enter these woods rarely come out alive. Good luck.\n\n";
 
-	map->roomsList[4].altDescription = "You are currently at the Morgue. An overgrown house that is home to those yet to be buried. You notice a padlock on the gate. Lost_Burials, Soul_Forest, and the Old_Chapel are nearby.\n\n";
+	map->roomsList[4].altDescription = "You are currently at the Morgue. An overgrown house that is home to those yet to be buried.\n\n";
 
 
 
@@ -111,6 +116,9 @@ struct Map* initMap() {
 	map->roomsList[4].nearbyRooms[2] = &(map->roomsList[5]);
 
 	map->roomsList[5].nearbyRooms[0] = &(map->roomsList[4]);
+	map->roomsList[5].nearbyRooms[1] = &(map->roomsList[6]);
+
+	map->roomsList[6].nearbyRooms[0] = &(map->roomsList[5]);
 
 
 	return map;
@@ -175,6 +183,10 @@ int main() {
 		}
 		if(strcmp(userInput, "inventory") == 0) {
 			inventory(gameMap);
+		}
+
+		if(strcmp(userInput, "run") == 0) {
+			run(gameMap);
 		}
 	}
 
@@ -252,7 +264,7 @@ void helpMenu() {
 }
 
 void printNearby(struct Map* map) {
-	printf("\nNearby rooms: ");
+	printf("\nNearby areas: ");
 	int i;
 	for(i = 0; i < NEARBY_ROOMS_MAX; i++) {
 		if(map->currentRoom->nearbyRooms[i] != NULL) {
@@ -265,7 +277,7 @@ void printNearby(struct Map* map) {
 
 void whereami(struct Map* map) {
 
-	printf("%s\n", map->currentRoom->description);
+	altDescCheck(map);
 }
 
 void inventory(struct Map* map) {
@@ -280,4 +292,54 @@ void inventory(struct Map* map) {
 		}
 	}
 	printf("\n\n");
+}
+
+void run(struct Map* map) {
+	if(strcmp(map->currentRoom->name, map->roomsList[3].name) == 0 ) {
+		map->currentRoom = &map->roomsList[2];
+		altDescCheck(map);
+		return;
+	}
+
+
+	if(strcmp(map->currentRoom->name, map->roomsList[2].name) == 0 ) {
+		map->currentRoom = &map->roomsList[0];
+		altDescCheck(map);
+		return;
+	}
+
+	if(strcmp(map->currentRoom->name, map->roomsList[5].name) == 0 ) {
+		map->currentRoom = &map->roomsList[4];
+		altDescCheck(map);
+		return;
+	}
+
+	if(strcmp(map->currentRoom->name, map->roomsList[6].name) == 0 ) {
+		printf("\nYou make a dash for the chapel doors but they are shut by some mysterious force. You are trapped. You are dead.\n");
+		map->currentRoom = &map->roomsList[0];
+		altDescCheck(map);
+		return;
+	}
+
+
+		
+}
+
+	/* REUSABLE CODE */
+void altDescCheck(struct Map* map) {
+	int j, k;
+
+	/* check for picked up items for alternate description */
+	for(k = 0; k < INV_SIZE_MAX; k++) {
+		if(map->currentRoom->items[k] != NULL) {
+			for(j = 0; j < INV_SIZE_MAX; j++) {
+				if(map->player.inventory[j] == map->currentRoom->items[k] && map->player.inventory[j] != NULL) {
+					printf("\n%s\n", map->currentRoom->altDescription);
+					return;
+				}
+			}
+		}
+	}
+	printf("%s\n", map->currentRoom->description);
+	return;
 }
